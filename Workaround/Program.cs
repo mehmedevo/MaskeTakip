d8932c64fe31bea478013a91c99325ccfc8af086
MONSTER <MONSTER@DESKTOP-K4F4KFM>
2024-02-06 23:34:19 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+namespace Workaround
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            SelamVer();
+            Console.ReadKey();
+            
+            
+            
+
+        }
+
+        static void SelamVer(string isim="isimsiz")
+        {
+            
+            Console.WriteLine("Merhaba" +" "+ isim);
+        }
+
+        public void mesela()
+        {
+            PttManager
+        }
+    }
+
+   
+}
---
+++ Workaround2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Business.Abstract;
+using Business.Concrete;
+using Entities.Concrete;
+
+
+namespace Workaround
+{
+    public class Program
+    {
+        private IApplicantService _applicantService;
+        static void Main(string[] args)
+        {
+
+            SelamVer();
+            
+            Person person1 = new Person();
+            person1.FirstName = "MEHMET OSMAN";
+            person1.LastName = "ŞENTÜRK";
+            person1.DateOfBirthYear = 1995;
+            person1.NationalIdentity = 123;
+
+            
+
+            PttManager pttmanager = new(new PersonManager());
+            pttmanager.GiveMask(person1);
+
+            
+            
+
+        }
+
+        static void SelamVer(string isim = "isimsiz")
+        {
+
+            Console.WriteLine("Merhaba" + " " + isim);
+        }
+
+
+
+
+    }
+}
